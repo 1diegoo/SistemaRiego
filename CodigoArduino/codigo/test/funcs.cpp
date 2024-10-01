6913273fcec95dbc01d217beffbb82ed09863ee1
WettiestMan <S_4r8ol@protonmail.com>
2024-10-01 09:23:15 -0500

    pequeños ajustes en los cálculos

--- CodigoArduino/codigo/test/funcs.cpp
+++ CodigoArduino/codigo/test/funcs.cpp
@@ -1,24 +1,30 @@
 #include <Arduino.h>
 #include <DHT.h>
-#include "funcs.h"
+#include "funcs.h"  |   
 #include "tipos.h"
 
 void leerDatos(DHT& dht, Lecturas& lecs) {
-  lecs.humedadAire = dht.readHumidity();
+  /*lecs.humedadAire = dht.readHumidity();
+  Serial.println(lecs.humedadAire);
   lecs.temperatura = dht.readTemperature();
+  Serial.println(lecs.temperatura);
+  delay(3000);
   if(isnan(lecs.humedadAire) || isnan(lecs.temperatura)) {
     Serial.println("Error en la deteccion de medidas, el sensor respondio con NaN");
     return;
-  }
+  }*/
 
-  lecs.humedadSuelo = (analogRead(A1)/1023.0) * 100;
-  lecs.luminosidad = (((1023 - analogRead(A0))/1023.0) * 100);
+  lecs.humedadSuelo = ((1023 - analogRead(A1))/1023.0) * 100;
+  Serial.println(lecs.humedadSuelo);
+  lecs.luminosidad = (((analogRead(A0))/1023.0) * 100);
+  Serial.println(lecs.luminosidad);
+  delay(3000);
   estadoActual = Estados::interpretando;
 }
 
 void interpretarDatos(Lecturas& lecs) {
   if(lecs.humedadSuelo < 50.0
-     && lecs.temperatura < 25
+     //&& lecs.temperatura < 25
      && lecs.luminosidad < 50) {
     Serial.println("Activando riego...");
     estadoActual = Estados::ejecutando;
